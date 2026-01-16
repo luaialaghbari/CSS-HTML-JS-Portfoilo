2a03dad9b1178eddae0b8741aba890e84d1d3458
luaialaghbari <louiyaseralag@gmail.com>
2026-01-16 22:02:40 +0300

    Initial clean commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
+
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+const repoName = 'CSS-HTML-JS-Portfoilo'; // Change if your repo name is different
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  output: 'export',
+  images: { unoptimized: true },
+  assetPrefix: isProd ? `/${repoName}/` : '',
+  basePath: isProd ? `/${repoName}` : '',
+};
 
 module.exports = nextConfig;
